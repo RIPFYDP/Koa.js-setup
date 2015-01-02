dadf15087ea6442abd755d0d7c7076580afd2866
Jason Kim <jasonkim@rhapsody.com>
2015-01-02 10:33:47 -0800

    added a logger

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var koa    = require('koa'),
     mount  = require('koa-mount'),
-    Router = require('koa-router');
+    Router = require('koa-router'),
+    logger = require('koa-logger');
 
 var app = module.exports = koa();
 
+// logging
+app.use(logger());
+
+// API versioning and routing
 var APIv0 = new Router();
 
 APIv0.get('/', function *(){
@@ -11,4 +16,6 @@ APIv0.get('/', function *(){
 });
 
 app.use(mount('/v0', APIv0.middleware()));
+
+// assign port number
 app.listen(5000);
